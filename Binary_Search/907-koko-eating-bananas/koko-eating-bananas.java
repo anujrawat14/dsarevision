85f6b0098697c90be81369fc84c383c2d6907aca
anujrawat14 <anujrawatff@gmail.com>
2026-07-19 09:42:13 +0530

    Time: 6 ms (99.63%) | Memory: 47.9 MB (45.67%) - LeetSync

--- Binary_Search/907-koko-eating-bananas/koko-eating-bananas.java
+++ Binary_Search/907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,39 +1,45 @@
 class Solution {
-    int maxelement(int[] arr, int n) {
+
+    public int maxelement(int[] arr, int n) {
         int max = arr[0];
         for (int i = 0; i < n; i++) {
-            if (max < arr[i]) {
+            if (arr[i] > max) {
                 max = arr[i];
             }
         }
         return max;
     }
 
+    public long totalhour(int piles[], int mid) {
+        long count = 0;
+
+        for (int i = 0; i < piles.length; i++) {
+            count += (piles[i] + mid - 1) / mid;
+        }
+
+        return count;
+    }
+
     public int minEatingSpeed(int[] piles, int h) {
-        int n = piles.length;
+
         int left = 1;
-        int right = maxelement(piles, n);
-        int ans =right;
+        int right = maxelement(piles, piles.length);
+        int ans = right;
+
         while (left <= right) {
-            int mid = left+(right-left) / 2;
 
-            long count = 0;
+            int mid = left + (right - left) / 2;
 
-            for (int i = 0; i < piles.length; i++) {
-                int calculate = (piles[i] + mid - 1) / mid;
-                count += calculate;
-            }
+            long totalhr = totalhour(piles, mid); // ✅ fixed
 
-            if (count <= h) {
+            if (totalhr <= h) {
                 ans = mid;
                 right = mid - 1;
-
             } else {
                 left = mid + 1;
             }
-
         }
-        return ans;
 
+        return ans;
     }
 }
