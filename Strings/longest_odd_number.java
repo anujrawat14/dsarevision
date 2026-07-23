b6a71ec11bc6090b56b2d6ad1ab528f57639303f
anujrawat14 <anujrawatff@gmail.com>
2026-07-23 10:40:57 +0530

    Create longest_odd_number.java

---
+++ Strings/longest_odd_number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String largestOddNumber(String num) {
+
+        for (int i = num.length() - 1; i >= 0; i--) {
+
+            char ch = num.charAt(i);
+
+            int c = ch - '0';
+
+            if (c % 2 == 1) {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+}
