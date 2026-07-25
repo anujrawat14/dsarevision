672fd501242cbaa3051dc1c1c510cc75926f870b
anujrawat14 <anujrawatff@gmail.com>
2026-07-25 08:35:27 +0530

    Create Valid_Anangram.java

---
+++ Strings/Valid_Anangram.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        // if (s.length() != t.length()) {
+        //     return false;
+        // }
+        // char[] arr1 = s.toCharArray();
+        // char[] arr2 = t.toCharArray();
+
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
+
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (arr1[i] != arr2[i]) {
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+
+      import java.util.ArrayList;
+
+
+
+         if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] alphabets = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            alphabets[s.charAt(i) - 'a']++;
+            alphabets[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (alphabets[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+  
+    }
+}
