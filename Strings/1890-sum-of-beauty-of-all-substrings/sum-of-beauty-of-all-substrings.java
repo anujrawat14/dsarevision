6de1eb21794227e66e4ef33247148681bca3f778
anujrawat14 <anujrawatff@gmail.com>
2026-07-26 09:49:39 +0530

    Time: 60 ms (78.23%) | Memory: 44.2 MB (47.00%) - LeetSync

--- Strings/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ Strings/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,31 +1,23 @@
 class Solution {
-    int beauty(String s) {
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            int index = s.charAt(i) - 'a';
-            freq[index]++;
-        }
-        int max = 0;
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) {
-                max = Math.max(max, freq[i]);
-                min = Math.min(min, freq[i]);
-            }
-        }
-
-        return max - min;
-    }
-
     public int beautySum(String s) {
-        int ans = 0;
+        int ans=0;
         for (int i = 0; i < s.length(); i++) {
+            int[] freq = new int[26];
+
             for (int j = i; j < s.length(); j++) {
-                ans += beauty(s.substring(i, j+1));
+                int index = s.charAt(j) - 'a';
+                freq[index]++;
+                int max = 0;
+                int min = Integer.MAX_VALUE;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        max = Math.max(max, freq[k]);
+                        min = Math.min(min, freq[k]);
+                    }
+                }
+                ans += max - min;
             }
         }
         return ans;
-
     }
 }
