434fdc0cad092d6f5e16c202ea1abb60e9789fe6
anujrawat14 <anujrawatff@gmail.com>
2026-07-26 09:56:04 +0530

    Update sum-of-beauty-of-all-substrings.java

--- Strings/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ Strings/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,4 +1,33 @@
 class Solution {
+        //brute
+        // int beauty(String s) {
+        // int[] freq = new int[26];
+        // for (int i = 0; i < s.length(); i++) {
+        //     int index = s.charAt(i) - 'a';
+        //     freq[index]++;
+        // }
+        // int max = 0;
+        // int min = Integer.MAX_VALUE;
+
+        // for (int i = 0; i < 26; i++) {
+        //     if (freq[i] > 0) {
+        //         max = Math.max(max, freq[i]);
+        //         min = Math.min(min, freq[i]);
+        //     }
+        // }
+
+        // return max - min;
+        // }
+
+        // int ans = 0;
+        // for (int i = 0; i < s.length(); i++) {
+        //     for (int j = i; j < s.length(); j++) {
+        //         ans += beauty(s.substring(i, j + 1));
+        //     }
+        // }
+        // return ans;
+
+    //optimize
     public int beautySum(String s) {
         int ans=0;
         for (int i = 0; i < s.length(); i++) {
