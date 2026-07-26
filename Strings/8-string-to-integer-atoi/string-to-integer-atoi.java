f0af8850753d63c68dbf922a0d9dd8a58e916e7f
anujrawat14 <anujrawatff@gmail.com>
2026-07-26 08:43:04 +0530

    Update string-to-integer-atoi.java

--- Strings/8-string-to-integer-atoi/string-to-integer-atoi.java
+++ Strings/8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,38 +1,54 @@
 class Solution {
-    public int myAtoi(String s) {
+    String removeWhitespace(String s){
+        return s.trim();
+    }
+    char signedfun(String s){
+        char sign='+';
+        if( s.length()>0 && s.charAt(0)=='-'){
+            sign='-';
+        }
+        return sign;
+    }
+    long number(String s){
+        int i=0;
 
-        s = s.trim();
-
-        if (s.length() == 0)
-            return 0;
-
-        int sign = 1;
-        int i = 0;
-
-        if (s.charAt(0) == '-') {
-            sign = -1;
-            i++;
-        } else if (s.charAt(0) == '+') {
+         if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
             i++;
         }
 
-       long result = 0;
+        long num=0;
 
-        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+        while(i<s.length()){
+            if(!Character.isDigit(s.charAt(i))){
+                break;
+            }
+            else{
+               num = num*10 + (s.charAt(i)-'0');
+                i++;
+            }
+        }
+        return num;
+    }
+    public int myAtoi(String input) {
+        // Your code goes here
+        String ans=removeWhitespace(input);
 
-            int digit = s.charAt(i) - '0';
+        char signed=signedfun(ans);
 
-            result = result * 10 + digit;
-
-            if (sign == 1 && result > Integer.MAX_VALUE)
-                return Integer.MAX_VALUE;
-
-            if (sign == -1 && -result < Integer.MIN_VALUE)
-                return Integer.MIN_VALUE;
-
-            i++;
+        long  result=number(ans);
+        
+        if(signed=='-'){
+            result=-result;
         }
 
-        return (int) (result * sign);
+    
+        
+        if(result>Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if(result<Integer.MIN_VALUE){
+            return Integer.MIN_VALUE;
+        }
+        return (int)  result;
     }
 }
