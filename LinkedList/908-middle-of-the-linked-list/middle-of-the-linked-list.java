d1925d7ff6c6e0ab436d3254b2ccefb289e19721
anujrawat14 <anujrawatff@gmail.com>
2026-07-31 10:23:37 +0530

    Update middle-of-the-linked-list.java

--- LinkedList/908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ LinkedList/908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -9,6 +9,35 @@
  * }
  */
 class Solution {
+    
+    //brute
+    //  public int length(ListNode head) {
+    //     if (head == null) {
+    //         return 0;
+    //     }
+    //     int count = 0;
+    //     ListNode temp = head;
+    //     while (temp != null) {
+    //         temp = temp.next;
+    //         count++;
+    //     }
+    //     return count;
+    // }
+
+    // public ListNode middleNode(ListNode head) {
+    //     int count=length(head);
+    //     int mid = count / 2;
+
+    //     ListNode temp = head;
+    //     for (int i = 0; i < mid; i++) {
+    //         temp = temp.next;
+    //     }
+
+    //     return temp;
+    // }
+
+    
+    //optimal fast and slow pointers
 
     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
