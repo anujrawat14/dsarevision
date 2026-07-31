61a6b385582a4c52f81c3f8cc3be8c58db5a9230
anujrawat14 <anujrawatff@gmail.com>
2026-07-31 10:49:07 +0530

    Update reverse-linked-list.java

--- LinkedList/singly_linkedlist/206-reverse-linked-list/reverse-linked-list.java
+++ LinkedList/singly_linkedlist/206-reverse-linked-list/reverse-linked-list.java
@@ -9,16 +9,26 @@
  * }
  */
 class Solution {
+    
+    //brute approach 
+    //using an stack ds 
+    //simply push value 
+    //pop value and establish a backward link
 
+    //optimal approach
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
         ListNode front;
         
         while(curr!=null){
+            //save the next node befor breaking link
             front=curr.next;
+            //change the direction of link 
             curr.next=prev;
+            //move prev pointer 
             prev=curr;
+            //move curr pointer
             curr=front;
         }
         return prev;
