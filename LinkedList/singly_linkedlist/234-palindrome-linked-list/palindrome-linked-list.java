2d7fe703e3094658db960eac09152dccb738f426
anujrawat14 <anujrawatff@gmail.com>
2026-08-01 09:32:44 +0530

    Time: 5 ms (43.04%) | Memory: 94.3 MB (80.31%) - LeetSync

--- LinkedList/singly_linkedlist/234-palindrome-linked-list/palindrome-linked-list.java
+++ LinkedList/singly_linkedlist/234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,3 +1,4 @@
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -8,30 +9,70 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- import java.util.Stack;
+import java.util.Stack;
 
 class Solution {
-   
-    public boolean isPalindrome(ListNode head) {
-        //push in stack
-        Stack<Integer> stack=new Stack<>();
-         ListNode temp=head;
-         while(temp!=null){
-            stack.push(temp.val);
-            temp=temp.next;
-         }
-        
-        //pop with comaprison
-        ListNode temp2=head;
+    ListNode reverse(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode front = null;
 
-        while(!stack.isEmpty()){
-            int ans= stack.pop();
-            if(temp2.val!=ans){
+        while (curr != null) {
+            front = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = front;
+        }
+        return prev;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+
+        //brute using stack
+        //push in stack
+        // Stack<Integer> stack=new Stack<>();
+        //  ListNode temp=head;
+        //  while(temp!=null){
+        //     stack.push(temp.val);
+        //     temp=temp.next;
+        //  }
+
+        // //pop with comaprison
+        // ListNode temp2=head;
+
+        // while(!stack.isEmpty()){
+        //     int ans= stack.pop();
+        //     if(temp2.val!=ans){
+        //         return false;
+        //     }
+        //     temp2=temp2.next;
+        // }
+
+        // return true;
+
+        //optimal
+        //1:- find middle
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //reverse the second half
+        ListNode NewHead = reverse(slow.next);
+
+        //check;
+        ListNode newTemp = NewHead;
+        ListNode temp = head;
+
+        while (newTemp != null ) {
+            if (temp.val != newTemp.val) {
                 return false;
             }
-            temp2=temp2.next;
+            temp = temp.next;
+            newTemp = newTemp.next;
         }
-
+        reverse(NewHead);
         return true;
     }
 }
