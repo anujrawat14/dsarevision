2de37b0e038cd5c73a75cb0973635e0ce10c091c
anujrawat14 <anujrawatff@gmail.com>
2026-08-01 10:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (90.30%) - LeetSync

--- LinkedList/singly_linkedlist/328-odd-even-linked-list/odd-even-linked-list.java
+++ LinkedList/singly_linkedlist/328-odd-even-linked-list/odd-even-linked-list.java
@@ -11,58 +11,59 @@
  */
 import java.util.ArrayList;
 
-
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         //brute will have an array to store value
-        ArrayList<Integer> arr=new ArrayList<>();
-        if(head==null){
+        //     ArrayList<Integer> arr=new ArrayList<>();
+        if (head == null) {
             return null;
         }
-        ListNode temp=head;
-        //odd
-        while(temp!=null){
-            arr.add(temp.val);
-            if(temp.next==null){
-                break;
-            }
-            temp=temp.next.next;
-        }
-        //even
-        ListNode temp2=head.next;
-        while(temp2!=null){
-            arr.add(temp2.val);
-            if(temp2.next==null){
-                break;
-            }
-            temp2=temp2.next.next;
+        //     ListNode temp=head;
+        //     //odd
+        //     while(temp!=null){
+        //         arr.add(temp.val);
+        //         if(temp.next==null){
+        //             break;
+        //         }
+        //         temp=temp.next.next;
+        //     }
+        //     //even
+        //     ListNode temp2=head.next;
+        //     while(temp2!=null){
+        //         arr.add(temp2.val);
+        //         if(temp2.next==null){
+        //             break;
+        //         }
+        //         temp2=temp2.next.next;
+        //     }
+
+        //     //put value in ll
+        //    ListNode temp3 =head;
+        // for(int i=0;i<arr.size();i++){
+        //     temp3.val=arr.get(i);
+        //     temp3=temp3.next;
+        // }
+
+        // return head;
+
+        //OPTIMAL
+
+        ListNode odd = head;
+        ListNode evenHead = head.next;
+        ListNode even = evenHead;
+
+        while (even != null && even.next != null) {
+
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
         }
 
-        //put value in ll
-       ListNode temp3 =head;
-        for(int i=0;i<arr.size();i++){
-            temp3.val=arr.get(i);
-            temp3=temp3.next;
-        }
-        
+        odd.next = evenHead;
+
         return head;
 
-
-    //     if (head.next.next != null) {
-    //         ListNode odd = head;
-    //         ListNode temp = head;
-    //         ListNode even = temp.next;
-    //         while (even != null && odd != null) {
-    //             //1 odd wala yhi pr rhega
-             
-    //             odd.val = odd.val;
-                
-    //             odd = odd.next;
-    //             even.val=odd.val;
-
-    //             even = even.next;
-    //         }
-
-    //     }
     }
 }
