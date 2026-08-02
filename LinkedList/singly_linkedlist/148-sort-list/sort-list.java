ff97dc30d1c723a15e559a405522da8a93947ce7
anujrawat14 <anujrawatff@gmail.com>
2026-08-02 10:58:38 +0530

    Time: 9 ms (97.51%) | Memory: 59.5 MB (37.96%) - LeetSync

--- LinkedList/singly_linkedlist/148-sort-list/sort-list.java
+++ LinkedList/singly_linkedlist/148-sort-list/sort-list.java
@@ -13,24 +13,74 @@ import java.util.Collections;
 import java.util.ArrayList;
 
 class Solution {
+   public ListNode middle(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head.next;
+
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+   public ListNode merge(ListNode left, ListNode right) {
+    ListNode dummy = new ListNode(-1);
+    ListNode temp = dummy;
+
+    while (left != null && right != null) {
+        if (left.val <= right.val) {
+            temp.next = left;
+            left = left.next;
+        } else {
+            temp.next = right;
+            right = right.next;
+        }
+        temp = temp.next;
+    }
+
+    if (left != null) {
+        temp.next = left;
+    } else {
+        temp.next = right;
+    }
+
+    return dummy.next;
+}
+
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ArrayList<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
-        while (temp != null) {
-            arr.add(temp.val);
-            temp = temp.next;
-        }
-        Collections.sort(arr);
-        temp=head;
-       
-        for (int i = 0; i < arr.size(); i++) {
-            temp.val = arr.get(i);
-            temp = temp.next;
-        }
-        return head;
+        //brute force :-make a array, then sort them give the value
+        // ArrayList<Integer> arr = new ArrayList<>();
+        // ListNode temp = head;
+        // while (temp != null) {
+        //     arr.add(temp.val);
+        //     temp = temp.next;
+        // }
+        // Collections.sort(arr);
+        // temp=head;
+
+        // for (int i = 0; i < arr.size(); i++) {
+        //     temp.val = arr.get(i);
+        //     temp = temp.next;
+        // }
+        // return head;
+
+        //using merge sort directly
+
+        //find midddle
+        ListNode mid = middle(head);
+        ListNode lefthead = head;
+        ListNode righthead = mid.next;
+        mid.next = null;
+
+        lefthead=sortList(lefthead);
+        righthead=sortList(righthead);
+        return merge(lefthead,righthead);
+
+        // split LL left and right half
 
     }
 }
