c595a6e32675d3cb45241507535febc851d3de7f
anujrawat14 <anujrawatff@gmail.com>
2026-08-02 11:06:49 +0530

    Time: 9 ms (97.51%) | Memory: 59.3 MB (62.60%) - LeetSync

--- LinkedList/singly_linkedlist/148-sort-list/sort-list.java
+++ LinkedList/singly_linkedlist/148-sort-list/sort-list.java
@@ -13,40 +13,40 @@ import java.util.Collections;
 import java.util.ArrayList;
 
 class Solution {
-   public ListNode middle(ListNode head) {
-    ListNode slow = head;
-    ListNode fast = head.next;
+    public ListNode middle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
 
-    while (fast != null && fast.next != null) {
-        slow = slow.next;
-        fast = fast.next.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
     }
 
-    return slow;
-}
-   public ListNode merge(ListNode left, ListNode right) {
-    ListNode dummy = new ListNode(-1);
-    ListNode temp = dummy;
+    public ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
 
-    while (left != null && right != null) {
-        if (left.val <= right.val) {
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                temp.next = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                right=right.next;
+            }
+            temp = temp.next;
+        }
+        if (left != null) {
             temp.next = left;
-            left = left.next;
         } else {
             temp.next = right;
-            right = right.next;
         }
-        temp = temp.next;
-    }
 
-    if (left != null) {
-        temp.next = left;
-    } else {
-        temp.next = right;
+        return dummy.next;
     }
-
-    return dummy.next;
-}
 
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null) {
@@ -76,9 +76,9 @@ class Solution {
         ListNode righthead = mid.next;
         mid.next = null;
 
-        lefthead=sortList(lefthead);
-        righthead=sortList(righthead);
-        return merge(lefthead,righthead);
+        lefthead = sortList(lefthead);
+        righthead = sortList(righthead);
+        return merge(lefthead, righthead);
 
         // split LL left and right half
 
