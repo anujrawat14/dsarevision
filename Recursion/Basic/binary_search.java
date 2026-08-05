2d748ee8cdafe8e168fa05bee90aa7a0c8ddcf9f
anujrawat14 <anujrawatff@gmail.com>
2026-08-05 12:23:38 +0530

    Update and rename binaru_search.java to binary_search.java

---
+++ Recursion/Basic/binary_search.java
@@ -0,0 +1,29 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+      int arr[]={0,1,2,3,4,5,6,7,8,9,10};
+      int target=8;
+      int left=0;
+      int right=arr.length-1;
+      int index=Bs(arr,target,left,right);
+      System.out.println(index);
+    }
+    static int Bs(int[] arr,int target,int left,int right){
+        int mid=left+(right-left)/2;
+        if(left>right){
+            //base condition
+            return -1;
+        }
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[mid]>target){
+           return Bs(arr,target,left,mid-1);
+        }
+        else{
+           return Bs(arr,target,mid+1,right);
+        }
+    }
+}
