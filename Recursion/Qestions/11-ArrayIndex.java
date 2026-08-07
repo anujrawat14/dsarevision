c7f1e3ff5651ee0f2595f51c916c3f42a4f98515
anujrawat14 <anujrawatff@gmail.com>
2026-08-07 14:08:09 +0530

    Update 11-ArrayIndex.java

--- Recursion/Qestions/11-ArrayIndex.java
+++ Recursion/Qestions/11-ArrayIndex.java
@@ -2,20 +2,27 @@
 // import java.util.ArrayList;
 // class Main {
 //     static ArrayList<Integer> index(int arr[],int target ,int s,ArrayList<Integer> res){
-        
+
+//         // Base case: reached end of array
 //         if(s==arr.length){
 //             return res;
 //         }
+
+//         // If current element matches target, store its index
 //         if (arr[s]==target){
 //             res.add(s);
 //         }
+
+//         // Continue searching in the remaining array
 //         return index(arr,target,s+1,res); 
 //     }
+
 //     public static void main(String[] args) {
 //         int arr[]={1,2,3,4,4,5,6};
 //         int target=4;
-//         // ArrayList<Integer> res=new ArrayList<Integer>();
-//         System.out.println( index(arr,target,0,new ArrayList<Integer>()));
+
+//         // Passing a newly created ArrayList
+//         System.out.println(index(arr,target,0,new ArrayList<Integer>()));
 //     }
 // }
 
@@ -24,49 +31,82 @@
 // Use this editor to write, compile and run your Java code online
 
 // class Main {
+//     // Global list to store all matching indices
 //     static ArrayList<Integer> resultant=new ArrayList<>();
+
 //     static void index(int arr[],int target ,int s){
-        
+
+//         // Base case
 //         if(s==arr.length){
 //             return ;
 //         }
+
+//         // Store index if target is found
 //         if (arr[s]==target){
 //             resultant.add(s);
 //         }
-//         index(arr,target,s+1); 
+
+//         // Move to next index
+//         index(arr,target,s+1);
 //     }
+
 //     public static void main(String[] args) {
 //        int arr[]={1,2,3,4,4,5,6};
 //        int target=4;
+
+//        // Start recursive search
 //        index(arr,target,0);
-       
+
+//        // Print all stored indices
 //        for(int i: resultant){
 //            System.out.println(i);
 //        }
-       
-//     }    
+
+//     }
 // }
 
 // EVRY FUNCTION RETURN INDIVIDUAL ANSWER AND ADD THEM
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
- import java.util.ArrayList;
+
+import java.util.ArrayList;
+
 class Main {
-    static ArrayList<Integer> index(int arr[],int t,int s){
-           ArrayList<Integer> list=new  ArrayList<Integer>() ;
-           if(s==arr.length){
-               return list;
-           }
-           if(arr[s]==t){
-               list.add(s);
-           }
-           ArrayList<Integer> ans=index(arr,t,s+1);
-           list.addAll(ans);
-           return list;
+
+    static ArrayList<Integer> index(int arr[], int t, int s) {
+
+        // Every recursive call creates its own list
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        // Base case: return empty list
+        if (s == arr.length) {
+            return list;
+        }
+
+        // If current element matches target,
+        // store the current index in this call's list
+        if (arr[s] == t) {
+            list.add(s);
+        }
+
+        // Get the answer from the remaining recursive calls
+        ArrayList<Integer> ans = index(arr, t, s + 1);
+
+        // Merge current list with the returned list
+        list.addAll(ans);
+
+        // Return the combined list
+        return list;
     }
+
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,4,5};
-        ArrayList<Integer> as=index(arr,4,0);
+
+        int arr[] = {1, 2, 3, 4, 4, 5};
+
+        // Start recursive search
+        ArrayList<Integer> as = index(arr, 4, 0);
+
+        // Print all indices
         System.out.println(as);
     }
 }
