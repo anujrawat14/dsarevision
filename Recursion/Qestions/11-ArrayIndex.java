33bdbc6d1ed59d65a86f9b39d6d855ed8c23347e
anujrawat14 <anujrawatff@gmail.com>
2026-08-07 13:17:18 +0530

    Update 11-ArrayIndex.java

--- Recursion/Qestions/11-ArrayIndex.java
+++ Recursion/Qestions/11-ArrayIndex.java
@@ -23,26 +23,50 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 
-class Main {
-    static ArrayList<Integer> resultant=new ArrayList<>();
-    static void index(int arr[],int target ,int s){
+// class Main {
+//     static ArrayList<Integer> resultant=new ArrayList<>();
+//     static void index(int arr[],int target ,int s){
         
-        if(s==arr.length){
-            return ;
-        }
-        if (arr[s]==target){
-            resultant.add(s);
-        }
-        index(arr,target,s+1); 
+//         if(s==arr.length){
+//             return ;
+//         }
+//         if (arr[s]==target){
+//             resultant.add(s);
+//         }
+//         index(arr,target,s+1); 
+//     }
+//     public static void main(String[] args) {
+//        int arr[]={1,2,3,4,4,5,6};
+//        int target=4;
+//        index(arr,target,0);
+       
+//        for(int i: resultant){
+//            System.out.println(i);
+//        }
+       
+//     }    
+// }
+
+// EVRY FUNCTION RETURN INDIVIDUAL ANSWER AND ADD THEM
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+ import java.util.ArrayList;
+class Main {
+    static ArrayList<Integer> index(int arr[],int t,int s){
+           ArrayList<Integer> list=new  ArrayList<Integer>() ;
+           if(s==arr.length){
+               return list;
+           }
+           if(arr[s]==t){
+               list.add(s);
+           }
+           ArrayList<Integer> ans=index(arr,t,s+1);
+           list.addAll(ans);
+           return list;
     }
     public static void main(String[] args) {
-       int arr[]={1,2,3,4,4,5,6};
-       int target=4;
-       index(arr,target,0);
-       
-       for(int i: resultant){
-           System.out.println(i);
-       }
-       
-    }    
+        int arr[]={1,2,3,4,4,5};
+        ArrayList<Integer> as=index(arr,4,0);
+        System.out.println(as);
+    }
 }
