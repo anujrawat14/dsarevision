211f5d2ffccfd9c566fae77ee4713a2d19aec182
anujrawat14 <anujrawatff@gmail.com>
2026-08-07 12:42:42 +0530

    Update 11-ArrayIndex.java

--- Recursion/Qestions/11-ArrayIndex.java
+++ Recursion/Qestions/11-ArrayIndex.java
@@ -1,5 +1,5 @@
 //adding arraylist inside function call
-import java.util.ArrayList;
+// import java.util.ArrayList;
 // class Main {
 //     static ArrayList<Integer> index(int arr[],int target ,int s,ArrayList<Integer> res){
         
@@ -12,14 +12,10 @@ import java.util.ArrayList;
 //         return index(arr,target,s+1,res); 
 //     }
 //     public static void main(String[] args) {
-//        int arr[]={1,2,3,4,4,5,6};
-//        int target=4;
-//        ArrayList<Integer> res=new ArrayList<Integer>();
-//        ArrayList<Integer> result= index(arr,target,0,res);
-//        for(int i: result){
-//            System.out.println(i);
-//        }
-       
+//         int arr[]={1,2,3,4,4,5,6};
+//         int target=4;
+//         // ArrayList<Integer> res=new ArrayList<Integer>();
+//         System.out.println( index(arr,target,0,new ArrayList<Integer>()));
 //     }
 // }
 
