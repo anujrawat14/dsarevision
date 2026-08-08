59f7f92011e1741ef88c98b48b871597edd7be31
anujrawat14 <anujrawatff@gmail.com>
2026-08-08 13:11:26 +0530

    Create 1-part1.java

---
+++ Recursion/Qestions/patterns/1-part1.java
@@ -0,0 +1,24 @@
+// * * * * 
+// * * * 
+// * * 
+// * 
+
+class Main {
+    
+static void    print(int r,int c){
+        if(r==0){
+            return;
+        }
+        if(r>c){
+            System.out.print("* ");
+            print(r,c+1);
+        }
+        else{
+            System.out.println();
+            print(r-1,0);
+        }
+    }
+    public static void main(String[] args) {
+       print(4,0);
+    }
+}
