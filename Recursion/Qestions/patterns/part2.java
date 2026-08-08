8d6194ca326ec8a0b956278e6b698d1791d3a00d
anujrawat14 <anujrawatff@gmail.com>
2026-08-08 13:29:02 +0530

    Create part2.java

---
+++ Recursion/Qestions/patterns/part2.java
@@ -0,0 +1,30 @@
+//* 
+// * * 
+// * * * 
+// * * * * 
+
+//USING TAIL RECURSION
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+// class Main {
+//       static void fun(int n){
+//          print(1,0,n);
+//        }    
+//       static void print(int r,int c,int n){
+//           if(r>n){
+//               return;
+//           }
+//         if(r>c){
+//             System.out.print("* ");
+//             print(r,c+1,n);
+//         }
+//         else{
+//             System.out.println();
+//             print(r+1,0,n);
+//         }
+//       }
+//     public static void main(String[] args) {
+//        fun(4);
+//     }
+// }
