1a549e511d3bc1fdb0c85a733ac5009bb487aa49
anujrawat14 <anujrawatff@gmail.com>
2026-08-09 08:50:14 +0530

    Update buubleSort.java

--- Recursion/Sort/buubleSort.java
+++ Recursion/Sort/buubleSort.java
@@ -1,36 +1,26 @@
-// IT IS SIMILAR TO PARTTERN 1 PART 1 JAVA
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+
 
 class Main {
-    static void BubbleSort(int[] arr){
-        sort(arr,arr.length,0);
-    }
-  static void swap(int[] arr, int l, int r) {
-        int temp = arr[l];
-        arr[l] = arr[r];
-        arr[r] = temp;
-    }
-
-    static void sort(int[] arr,int r,int c){
-        if(r==0){
-            return ;
-        }
-        if(r==c){
-            sort(arr,r-1,0);
+    static void bubleSort(int arr[],int row,int col){
+        if(row==0){
             return;
         }
-         if(arr[c]>arr[c+1]){
-                swap(arr,c,c+1);
-           }
-         sort(arr,r,c+1);
+        if(row>col){
+            if(arr[col]>arr[col+1]){
+                //swap
+                int temp=arr[col];
+                arr[col]=arr[col+1];
+                arr[col+1]=temp;
+            }
+             bubleSort(arr,row,col+1);
+        }
+        bubleSort(arr,row-1,0);
     }
     public static void main(String[] args) {
-        int arr[]={3, 5, 8, 1, 2};
-        BubbleSort(arr);
-        for(int i=0;i<arr.length;i++){
-        
-        System.out.println(arr[i]);
-        }
+        int arr[]={5,6,1,3};
+       bubleSort(arr,arr.length-1,0);
+       for(int i=0;i<arr.length;i++){
+         System.out.println(arr[i]);
+       }
     }
 }
