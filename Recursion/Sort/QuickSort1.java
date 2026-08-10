77e5ab95566735bbb01651a7e820dac10f47e28e
anujrawat14 <anujrawatff@gmail.com>
2026-08-10 11:22:03 +0530

    Create QuickSort1.java

---
+++ Recursion/Sort/QuickSort1.java
@@ -0,0 +1,42 @@
+//quick sort code
+class Main {
+    static void quickSort(int arr[],int low,int high){
+        if(low >= high){
+            return;
+        }
+        int s=low;int e=high-1;int pivot=arr[high];
+        
+        while(s<=e){
+            //if array sorted toh hum ko swap nhi krn hai
+           while (s <= e && arr[s] <= pivot) {
+                s++;
+            }
+            while(s<=e && arr[e] > pivot){
+                e--;
+            }
+            if(s<=e){
+                int temp=arr[s];
+                arr[s]=arr[e];
+                arr[e]=temp;
+                s++;e--;
+            }
+        }
+        // pivot ko correct position par rakhenge
+        int temp = arr[s];
+        arr[s] = arr[high];
+        arr[high] = temp;
+        quickSort(arr,low,e);
+        quickSort(arr,s,high);
+    }
+    public static void main(String[] args) {
+        int arr[] = {7, 6, 5, 4};
+         // Quick Sort call
+         quickSort(arr, 0, arr.length-1); 
+         // Print sorted array 
+         for (int i = 0; i < arr.length; i++) {
+           System.out.println(arr[i]);
+          } 
+        
+    
+    }
+}
