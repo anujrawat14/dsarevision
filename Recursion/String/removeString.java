dd5e72362518817d9f92774c8a80f23e7c736f28
anujrawat14 <anujrawatff@gmail.com>
2026-08-11 09:44:37 +0530

    Update removeString.java

--- Recursion/String/removeString.java
+++ Recursion/String/removeString.java
@@ -1,20 +1,45 @@
+//remove a string
+
+// class Main {
+//     static String skip(String str){
+//       //if string == apple skip
+//       if(str.isEmpty()){
+//           return "";
+//       }
+//       if(str.startsWith("apple")){
+//           return skip(str.substring(5));
+//       }
+//       else{
+//           return  str.charAt(0)+skip(str.substring(1));
+//       }
+//     }
+//     public static void main(String[] args) {
+//         String res=skip("applebbbbapple");
+//         System.out.println(res);
+//     }
+// }
 
 
+//remove a string with only few characters
 class Main {
-    static String skip(String str){
-      //if string == apple skip
-      if(str.isEmpty()){
-          return "";
-      }
-      if(str.startsWith("apple")){
-          return skip(str.substring(5));
-      }
-      else{
-          return  str.charAt(0)+skip(str.substring(1));
-      }
+
+    static String skipAppNotApple(String str) {
+
+        if (str.isEmpty()) {
+            return "";
+        }
+
+        // Skip "app" only if it is NOT part of "apple"
+        if (str.startsWith("app") && !str.startsWith("apple")) {
+            return skipAppNotApple(str.substring(3));
+        } 
+        else {
+            return str.charAt(0) + skipAppNotApple(str.substring(1));
+        }
     }
+
     public static void main(String[] args) {
-        String res=skip("applebbbbapple");
+        String res = skipAppNotApple("appbbbbappe");
         System.out.println(res);
     }
 }
