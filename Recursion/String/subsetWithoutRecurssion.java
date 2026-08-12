3ed2e3e6bad15b99a34b234c2bbecf7e71fc4926
anujrawat14 <anujrawatff@gmail.com>
2026-08-12 09:20:03 +0530

    Update subsetWithoutRecurssion.java

--- Recursion/String/subsetWithoutRecurssion.java
+++ Recursion/String/subsetWithoutRecurssion.java
@@ -1,3 +1,28 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+// import java.util.*;
+// class Main {
+//     static List<List<Integer>> subset(int[] arr){
+//        List<List<Integer>> outerList=new ArrayList<>();
+//        outerList.add(new ArrayList<Integer>());
+
+//        for(int num:arr){
+//         int n=outerList.size();
+//         for(int i=0;i<n;i++){
+//             List<Integer> innerList=new ArrayList<>(outerList.get(i));
+//             innerList.add(num);
+//             outerList.add(innerList);
+//         }
+//        }
+//        return outerList;
+        
+//     }
+//     public static void main(String[] args) {
+//         int[] arr={1,2,3};
+//         System.out.println(subset(arr));
+//     }
+// }
+
 import java.util.*;
 
 class Main {
