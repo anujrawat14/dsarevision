cd7ed1402c4bed2c220988b9b5ca0dec69823fb1
anujrawat14 <anujrawatff@gmail.com>
2026-08-13 09:58:19 +0530

    Time: 3 ms (29.38%) | Memory: 49.1 MB (13.98%) - LeetSync

---
+++ Recursion/Qestions/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+
+    public List<String> helper(String p, String up) {
+
+        //base condition
+        if (up.isEmpty()) {
+            List<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        int digit = up.charAt(0) - '0';//it will give me integer of that digit like  12"->1 
+
+        int start = (digit - 2) * 3;
+        int end = (digit - 1) * 3;
+
+        if (digit >= 8) {
+            start++;
+            end++;
+        }
+
+        if (digit == 7 || digit == 9) {
+            end = end + 1;
+        }
+        List<String> ans = new ArrayList<>();
+
+        for (int i = start; i < end; i++) {
+
+            char ch = (char) ('a' + i);
+
+            ans.addAll(helper(p + ch, up.substring(1)));
+
+        }
+
+        return ans;
+    }
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return helper("", digits);
+    }
+}
