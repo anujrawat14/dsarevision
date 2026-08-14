1e907829a7fb8d783da6c7930462afb1edec14f0
anujrawat14 <anujrawatff@gmail.com>
2026-08-14 09:20:42 +0530

    Create makingStringfromDIgit.java

---
+++ Recursion/String/makingStringfromDIgit.java
@@ -0,0 +1,66 @@
+//without storing values
+import java.util.ArrayList;
+import java.util.List;
+
+class test {
+
+    static List<String> subset(String p, String up) {
+        if (up.isEmpty()) {
+            List<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        int digit = (int) (up.charAt(0) - '0');// '2'->2
+        List<String> newList = new ArrayList<>();
+        int start = (digit - 2) * 3;
+        int end = (digit - 1) * 3;
+
+        if (digit < 2) {
+            return new ArrayList<>();
+        }
+        // if (digit == 7 ) {
+        // end = end + 1;//4 charcter a rha
+        // }
+        // if(digit==8){
+        // start=start+1;
+        // end=end+1;
+        // }
+        // if(digit==9){
+        // start=start+1;
+        // end=end+2;
+        // }
+        switch (digit) {
+            case 7:
+                end = end + 1;
+                break;
+            case 8:
+                start = start + 1;
+                end = end + 1;
+                break;
+            case 9:
+                start = start + 1;
+                end = end + 2;
+                break;
+            default:
+                start = start;
+                end = end;
+        }
+        for (int i = start; i < end; i++) {
+            char ch = (char) ('a' + i);
+            newList.addAll(subset(p + ch, up.substring(1)));
+        }
+
+        return newList;
+    }
+
+    static List<String> letter(String digit) {
+        return subset("", digit);
+    }
+
+    public static void main(String args[]) {
+        List<String> res = letter("9");
+        System.out.println(res);
+
+    }
+}
