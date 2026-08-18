1708b05282778ebd6cf1d307e5d864b90137ceb7
anujrawat14 <anujrawatff@gmail.com>
2026-08-18 10:19:11 +0530

    Update NQueen2.java

--- Recursion/Backtarcking/NQueen2.java
+++ Recursion/Backtarcking/NQueen2.java
@@ -5,26 +5,24 @@ public class text1 {
         // base condition
         if (row == matrix.length) {
             display(matrix);
+            System.out.println();
             return 1;
         }
 
         int count = 0;
 
-        // loop to go to each column
-
+        // go to each column
         for (int col = 0; col < matrix[0].length; col++) {
 
             // check before placing
             if (isSafe(matrix, row, col)) {
 
-                matrix[row][col] = true;// place
+                matrix[row][col] = true; // place
 
-                count = count + Nqueen(matrix, row + 1);// move
+                count += Nqueen(matrix, row + 1); // move
 
-                matrix[row][col] = false;// unplace
-
+                matrix[row][col] = false; // unplace / backtrack
             }
-
         }
 
         return count;
@@ -32,33 +30,38 @@ public class text1 {
 
     static boolean isSafe(boolean[][] matrix, int row, int col) {
 
-        // verticall up row
-
+        // vertical up
         for (int i = row - 1; i >= 0; i--) {
+
             if (matrix[i][col]) {
                 return false;
             }
         }
 
-        // upside right diagonal
+        // upper-right diagonal
         int i = row - 1;
         int j = col + 1;
 
-        while (i >= 0 && j <= matrix.length - 1) {
+        while (i >= 0 && j < matrix[0].length) {
+
             if (matrix[i][j]) {
                 return false;
             }
+
             i--;
             j++;
         }
 
-        // upside left diagonal
+        // upper-left diagonal
         i = row - 1;
         j = col - 1;
+
         while (i >= 0 && j >= 0) {
+
             if (matrix[i][j]) {
                 return false;
             }
+
             i--;
             j--;
         }
@@ -67,24 +70,29 @@ public class text1 {
     }
 
     static void display(boolean[][] matrix) {
+
         for (int row = 0; row < matrix.length; row++) {
+
             for (int col = 0; col < matrix[0].length; col++) {
+
                 if (matrix[row][col]) {
                     System.out.print("Q ");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("X ");
                 }
             }
+
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
+
         boolean matrix[][] = {
-                { false, false, false, false },
-                { false, false, false, false },
-                { false, false, false, false },
-                { false, false, false, false }
+                {false, false, false, false},
+                {false, false, false, false},
+                {false, false, false, false},
+                {false, false, false, false}
         };
 
         int ans = Nqueen(matrix, 0);
