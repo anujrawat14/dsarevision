8392578f830060324153847caa6fe6f840eac2de
anujrawat14 <anujrawatff@gmail.com>
2026-08-21 10:31:51 +0530

    Time: 1 ms (99.99%) | Memory: 45.9 MB (33.65%) - LeetSync

--- Recursion/Qestions/39-combination-sum/combination-sum.java
+++ Recursion/Qestions/39-combination-sum/combination-sum.java
@@ -1,28 +1,31 @@
 class Solution {
-   void helper(List<List<Integer>> ans,List<Integer> p,int[] up,int target,int start){
+    void helper(List<List<Integer>> ans, List<Integer> p, int[] up, int target, int start) {
 
         //base condition
-        if(target==0){
+        if (target == 0) {
             //process array haui usko list ka andar dal dunga
             ans.add(new ArrayList<>(p));
             return;
         }
 
-        if(target<0){
-            return;
+        //process array ka andar hum elemnt dalenga from unprocess array
+        for (int i = start; i < up.length; i++) {
+
+            if (up[i] > target) {
+                continue;
+            }
+            p.add(up[i]);
+            helper(ans, p, up, target - up[i], i);
+            p.remove(p.size() - 1);
+
         }
 
-        //process array ka andar hum elemnt dalenga from unprocess array
-        for(int i=start;i<up.length;i++){
-            p.add(up[i]);
-            helper(ans,p,up,target-up[i],i);
-            p.remove(p.size()-1);
-        }
     }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> p=new ArrayList<>();
-        helper(ans,p,candidates,target,0);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> p = new ArrayList<>();
+        helper(ans, p, candidates, target, 0);
         return ans;
     }
 }
