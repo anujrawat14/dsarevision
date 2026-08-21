5bfd1b722f988c9171a4d6c7692261a29d0389e9
anujrawat14 <anujrawatff@gmail.com>
2026-08-21 10:46:24 +0530

    Time: 3 ms (33.79%) | Memory: 45.8 MB (51.54%) - LeetSync

--- Recursion/Qestions/39-combination-sum/combination-sum.java
+++ Recursion/Qestions/39-combination-sum/combination-sum.java
@@ -11,11 +11,20 @@ class Solution {
         //process array ka andar hum elemnt dalenga from unprocess array
         for (int i = start; i < up.length; i++) {
 
-            if (up[i] > target) {
+              if (i > start && up[i] ==up[i - 1]) {
                 continue;
             }
+
+            if (up[i] > target) {
+                break;
+            }
+            //add krega
             p.add(up[i]);
+
+            //wapsa sa hum usi index sa strt krenga
             helper(ans, p, up, target - up[i], i);
+
+            //backtrack last wala ko remove krka
             p.remove(p.size() - 1);
 
         }
@@ -25,6 +34,7 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> p = new ArrayList<>();
+        Arrays.sort(candidates);
         helper(ans, p, candidates, target, 0);
         return ans;
     }
